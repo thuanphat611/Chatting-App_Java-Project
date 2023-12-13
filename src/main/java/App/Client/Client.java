ddd0eaa5356df39d8947594acd2a2cb9fe8c1baf
Thuận Phát <phat06112003@gmail.com>
2023-12-13 17:31:05 +0700

    implement frame close operation

--- src/main/java/App/Client/Client.java
+++ src/main/java/App/Client/Client.java
@@ -3,15 +3,25 @@ import App.Client.UI.*;
 import App.Client.Controller.*;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Client extends JFrame {
     public void createAndShowGUI() {
         setTitle("Chatting App");
         setDefaultLookAndFeelDecorated(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         Controller controller = new Controller();
         JPanel newContentPane = new HomePanel(this, controller);
+
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                controller.close();
+                System.exit(0);
+            }
+        });
 
         newContentPane.setOpaque(true);
         this.setContentPane(newContentPane);
