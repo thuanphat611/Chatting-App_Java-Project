61c937a3a71098f70167b6c440069f490e70264d
Thuận Phát <phat06112003@gmail.com>
2023-12-16 18:49:49 +0700

    add method addMemberToGroup, getGroups to Database

--- src/main/java/App/Server/Database/Database.java
+++ src/main/java/App/Server/Database/Database.java
@@ -1,8 +1,10 @@
 package App.Server.Database;
 
+import App.Client.Controller.Receiver;
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Database {
     private static String user = "sa";
@@ -15,6 +17,8 @@ public class Database {
     private  PreparedStatement registerStmt;
     private PreparedStatement groupNameCheckStmt;
     private PreparedStatement createGroupStmt;
+    private  PreparedStatement addMemberStmt;
+    private PreparedStatement getGroupStmt;
 
     public Database() {
         conn = null;
@@ -22,13 +26,18 @@ public class Database {
         try {
             DriverManager.registerDriver(new SQLServerDriver());
             conn = DriverManager.getConnection(dbURL);
-
-            usernameCheckStmt = conn.prepareStatement("SELECT * FROM ACCOUNTS WHERE USERNAME = ?");
-            loginStmt = conn.prepareStatement("SELECT * FROM ACCOUNTS WHERE USERNAME = ? AND PASSWORD = ?");
-            registerStmt = conn.prepareStatement("INSERT ACCOUNTS (USERNAME, PASSWORD) VALUES (?, ?)");
-            groupNameCheckStmt = conn.prepareStatement("SELECT * FROM GROUPS WHERE OWNER = ? AND GROUP_NAME = ?");
-            createGroupStmt = conn.prepareStatement("INSERT GROUPS (GROUP_NAME, OWNER) VALUES (?, ?)");
-
+            if (conn != null) {
+                usernameCheckStmt = conn.prepareStatement("SELECT * FROM ACCOUNTS WHERE USERNAME = ?");
+                loginStmt = conn.prepareStatement("SELECT * FROM ACCOUNTS WHERE USERNAME = ? AND PASSWORD = ?");
+                registerStmt = conn.prepareStatement("INSERT ACCOUNTS (USERNAME, PASSWORD) VALUES (?, ?)");
+                groupNameCheckStmt = conn.prepareStatement("SELECT * FROM GROUPS WHERE OWNER = ? AND GROUP_NAME = ?");
+                createGroupStmt = conn.prepareStatement("INSERT GROUPS (GROUP_NAME, OWNER) VALUES (?, ?)");
+                addMemberStmt = conn.prepareStatement("INSERT GROUP_MEMBERS (GROUP_NAME, OWNER, MEMBER_NAME) VALUES (?, ?, ?)");
+                getGroupStmt = conn.prepareStatement("SELECT * FROM GROUP_MEMBERS WHERE MEMBER_NAME = ?");
+            }
+            else {
+                System.out.println("Database: error creating connection");
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -71,6 +80,23 @@ public class Database {
         return result;
     }
 
+    public boolean addMemerToGroup(String groupName, String owner, String memberName) {
+        if (groupNameCheck(owner, groupName)) { //check if group exists, true -> not exists
+            return false;
+        }
+        try {
+            addMemberStmt.setString(1, groupName);
+            addMemberStmt.setString(2, owner);
+            addMemberStmt.setString(3, memberName);
+            addMemberStmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
     public boolean createGroup(String username, String groupName) {
         if (!groupNameCheck(username, groupName)) {
             return false;
@@ -79,6 +105,7 @@ public class Database {
             createGroupStmt.setString(1, groupName);
             createGroupStmt.setString(2, username);
             createGroupStmt.executeUpdate();
+            addMemerToGroup(groupName,username, username);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
@@ -116,11 +143,43 @@ public class Database {
         return true;
     }
 
+    public String[] getGroups(String username) {
+        String[] result = null;
+        try {
+            getGroupStmt.setString(1, username);
+            ResultSet rs = getGroupStmt.executeQuery();
+            if (rs == null)
+                return null;
+
+            ArrayList<String> groups = new ArrayList<>();
+
+            while (rs.next()) {
+                String groupName = rs.getString("GROUP_NAME");
+                groups.add(groupName);
+            }
+            if (groups.isEmpty())
+                return null;
+
+            result = new String[groups.size()];
+
+            for (int i = 0; i < groups.size(); i++)
+                result[i] = groups.get(i).toString();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
     //test Database's method
     public static void main(String[] args) {
         Database db1 = new Database();
+//        String[] res = db1.getGroups("admin");
+//        for (String i : res)
+//            System.out.println(i);
 //        System.out.println(db1.register("admin", "123456"));
 //        db1.createGroup("admin", "test group");
 //        System.out.println(db1.groupNameCheck("admin", "test group"));
     }
 }
+//TODO when leaving group, check if the group has no member. If true, delete the group
