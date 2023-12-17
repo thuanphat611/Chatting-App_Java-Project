9e4382570d429f2d297518e8a0c98e83886e344b
Thuận Phát <phat06112003@gmail.com>
2023-12-17 16:29:14 +0700

    update database script

---
+++ src/main/java/App/Client/UI/ChatPanel.java
@@ -0,0 +1,6 @@
+package App.Client.UI;
+
+import javax.swing.*;
+//TODO maximum message length is
+public class ChatPanel extends JPanel {
+}
--- src/main/java/App/Client/UI/MainPanel.java
+++
@@ -1,6 +0,0 @@
-package App.Client.UI;
-import javax.swing.*;
-
-public class MainPanel extends JPanel {
-
-}
